33d6cefd847a62ff02775246716b857081f586cd
ignazioselvaggio <ignazio.selvaggio@gmail.com>
2023-11-23 19:18:26 +0100

    1. esempio errore

--- TestMaven/src/main/java/Start.java
+++ TestMaven/src/main/java/Start.java
@@ -4,7 +4,7 @@ public class Start {
 	public static void main(String[] args) {
 		System.out.println("Ciao");
 		System.out.println("Ciao2");
-		System.out.println("Ciao3");
+		Syste.out.println("Ciao3");
 	}
 
 }
