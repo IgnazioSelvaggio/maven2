4d4f6f436b0387c53e1bccebe54a7e1e91661f7c
ignazioselvaggio <ignazio.selvaggio@gmail.com>
2023-11-23 19:19:23 +0100

    1. fix

--- TestMaven/src/main/java/Start.java
+++ TestMaven/src/main/java/Start.java
@@ -4,7 +4,8 @@ public class Start {
 	public static void main(String[] args) {
 		System.out.println("Ciao");
 		System.out.println("Ciao2");
-		Syste.out.println("Ciao3");
+		System.out.println("Ciao3");
+		System.out.println("Ciao4");
 	}
 
 }
