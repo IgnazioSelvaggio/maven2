caa53444038c5ecb9041bbdf1087029ec77db2ee
IgnazioSelvaggio <49954442+IgnazioSelvaggio@users.noreply.github.com>
2024-12-01 10:45:20 +0100

    commit 6

--- TestMaven/src/main/java/Start.java
+++ TestMaven/src/main/java/Start.java
@@ -6,7 +6,8 @@ public class Start {
 		System.out.println("Ciao2");
 		System.out.println("Ciao3");
 		System.out.println("Ciao4");
-		System.out.println("Ciao5);
+		System.out.println("Ciao5");
+		System.out.println("Ciao6");
 	}
 
 }
